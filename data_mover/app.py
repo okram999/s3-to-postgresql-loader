70fab861d319e84bcfb55df256309f65c43e2d90
Niris Okram <niris@amazon.com>
2023-12-14 21:56:52 -0600

    formatted the file

--- data_mover/app.py
+++ data_mover/app.py
@@ -7,6 +7,7 @@ s3_src_bucket = "medigate-bucket-600669894233"
 aws_region_name = "us-east-1"
 target_db_table = "public.diabetes"
 
+
 def get_secret():
     credential = {}
     secret_name = "medigate-demo"
@@ -14,28 +15,22 @@ def get_secret():
 
     # Create a Secrets Manager client
     session = boto3.session.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
-    )
+    client = session.client(service_name="secretsmanager", region_name=region_name)
     try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
-        )
+        get_secret_value_response = client.get_secret_value(SecretId=secret_name)
     except ClientError as e:
         raise e
 
     # Decrypts secret using the associated KMS key.
-    secret = json.loads(get_secret_value_response['SecretString'])
+    secret = json.loads(get_secret_value_response["SecretString"])
 
-    credential['username'] = secret['username']
-    credential['password'] = secret['password']
-    credential['host'] = secret['host']
-    credential['port'] = secret['port']
-    credential['dbInstanceIdentifier'] = secret['dbInstanceIdentifier']
+    credential["username"] = secret["username"]
+    credential["password"] = secret["password"]
+    credential["host"] = secret["host"]
+    credential["port"] = secret["port"]
+    credential["dbInstanceIdentifier"] = secret["dbInstanceIdentifier"]
     print(credential)
     return credential
-
 
 
 def lambda_handler(event, context):
@@ -43,10 +38,15 @@ def lambda_handler(event, context):
     print("retrieving secrets")
     credential = get_secret()
     print("retrieving secrets done..")
-    conn = pg8000.native.Connection(user=credential['username'], password=credential['password'], host=credential['host'], database="postgres", ssl_context=True)
+    conn = pg8000.native.Connection(
+        user=credential["username"],
+        password=credential["password"],
+        host=credential["host"],
+        database="postgres",
+        ssl_context=True,
+    )
     key = event["Records"][0]["s3"]["object"]["key"]
     query = f"SELECT aws_s3.table_import_from_s3('{target_db_table}','','(format csv, header true)',aws_commons.create_s3_uri('{s3_src_bucket}', '{key}', '{aws_region_name}'));"
-    # query = "SELECT aws_s3.table_import_from_s3('public.diabetes','','(format csv, header true)',aws_commons.create_s3_uri('medigate-bucket-600669894233', 'diabetes_2.csv', 'us-east-1'));"
     res = conn.run(query)
     print(res)
     conn.close()
